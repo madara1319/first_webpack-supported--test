7fda7a0c0224a858c830f5ce9b29b4e16e460b2d
madara1319 <mateusz.bik@op.pl>
2022-07-27 20:37:56 +0200

    new file: Mateusz.html

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/Mateusz.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "Mateusz.html",
+    }),
   ],
 };
